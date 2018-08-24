0e338b99f50a0405bc19c32e899eb3e2efbe5b12
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-08-23 20:56:43 -0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,11 +1,6 @@
 require 'redmine'
 
-#Rails.configuration.to_prepare do
-#  require_dependency 'redmine_pull_requests/patches/...'
-#end
-
-# Configure our plugin
-Redmine::Plugin.register :code_audit do
+Redmine::Plugin.register :redmine_pulls do
   name 'Pull Requests'
   author 'eXolnet'
   description 'Allows users to create pull requests for repositories linked to projects.'
@@ -13,9 +8,13 @@ Redmine::Plugin.register :code_audit do
   url 'https://github.com/eXolnet/redmine-pull-requests'
   author_url 'https://www.exolnet.com'
 
-  #project_module :audits do
-  #  permission :view_audits, { :audits => [:index] }
-  #end
+  requires_redmine :version_or_higher => '2.3'
 
   menu :project_menu, :pulls, { :controller => 'pulls', :action => 'index' }, :caption => 'Pull requests', :after => :issues, :param => :project_id
+
+  project_module :pulls do
+    permission :view_pulls, { :pulls => [:index] }
+  end
 end
+
+require 'redmine_pulls'
