a0db28da09b1136962fcee8712b01ea30e68c865
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-10-05 15:53:56 -0400

    Remove core patches for the to_prepare method

--- lib/redmine_pulls.rb
+++ lib/redmine_pulls.rb
@@ -1,15 +1,10 @@
-Rails.configuration.to_prepare do
-  require 'redmine_pulls/patches/abstract_adapter_helper'
-  require 'redmine_pulls/patches/application_helper_patch'
-  require 'redmine_pulls/patches/git_adapter_helper'
-  require 'redmine_pulls/patches/journal_patch'
-  require 'redmine_pulls/patches/queries_helper_patch'
-  require 'redmine_pulls/patches/routes_helper_patch'
+require 'redmine_pulls/patches/abstract_adapter_helper'
+require 'redmine_pulls/patches/application_helper_patch'
+require 'redmine_pulls/patches/git_adapter_helper'
+require 'redmine_pulls/patches/journal_patch'
+require 'redmine_pulls/patches/queries_helper_patch'
+require 'redmine_pulls/patches/routes_helper_patch'
 
-  if Redmine::Plugin.installed? :redmine_git_hosting
-    require 'redmine_pulls/patches/xitolite_adapter_adapter'
-  end
-end
-
-module RedminePulls
+if Redmine::Plugin.installed? :redmine_git_hosting
+  require 'redmine_pulls/patches/xitolite_adapter_adapter'
 end
