842d0a7a17912e067ef30abcf41fd8517278ffc4
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-10-10 15:05:48 -0400

    Move the refresh logic into the RefreshService

--- lib/redmine_pulls/services/refresh_service.rb
+++ lib/redmine_pulls/services/refresh_service.rb
@@ -1,6 +1,6 @@
 module RedminePulls
   module Services
-    class PullRefreshService < PullBaseService
+    class RefreshService < BaseService
       def execute
         return if @pull.closed?
 
@@ -15,27 +15,34 @@ module RedminePulls
       private
 
       def close_when_head_is_missing
-        @pull.close unless @pull.head_branch_exists?
+        return if @pull.head_branch_exists?
+
+        puts "close"
+        @pull.close
       end
 
       def detect_new_commits
-        return if pull.closed?
+        return if @pull.closed? || @pull.branch_missing?
 
-        commit_base_revision = @pull.repository.merge_base(@pull.commit_base, @pull.commit_head)
+        # First, detect the last commit in the head branch
         commit_head_revision = @pull.repository.scm.revisions(nil, @pull.commit_base, @pull.commit_head).collect {|revision| revision.identifier}.first
-
-        @pull.commit_base_revision = commit_base_revision unless commit_base_revision.blank?
         @pull.commit_head_revision = commit_head_revision unless commit_head_revision.blank?
+
+        return if @pull.repository.is_ancestor? @pull.commit_head_revision, @pull.commit_base
+
+        # Next, detect the first ancestor of both branches
+        commit_base_revision = @pull.repository.scm.merge_base(@pull.commit_base, @pull.commit_head)
+        @pull.commit_base_revision = commit_base_revision unless commit_base_revision.blank?
       end
 
       def detect_manually_merged
-        return if pull.closed?
+        return if @pull.closed? || @pull.commit_head_revision.blank?
 
         @pull.mark_as_merged if @pull.repository.is_ancestor? @pull.commit_head_revision, @pull.commit_base
       end
 
       def reload_merge_status
-        return unless pull.merge_status == 'unchecked'
+        return unless @pull.merge_status == 'unchecked'
 
         if ! @pull.base_branch_exists? || ! @pull.commit_base_revision
           @pull.mark_as_unmergeable
