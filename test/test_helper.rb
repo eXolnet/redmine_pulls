3bcd63ceca969c942f05b96f3d0e6d14ab65e526
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-10-11 21:49:17 -0400

    Add pulls fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,5 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
+# Enable project fixtures
+ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/', [:pulls])
