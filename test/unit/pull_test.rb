b3c7887c01393fb93dedfa4c9a3da10a17ea7d22
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-10-18 17:45:31 -0400

    Add some unit tests

--- test/unit/pull_test.rb
+++ test/unit/pull_test.rb
@@ -12,6 +12,10 @@ class PullTest < ActiveSupport::TestCase
            :changesets,
            :changes
 
+  def teardown
+    User.current = nil
+  end
+
   def test_initialize
     pull = Pull.new
 
@@ -28,14 +32,18 @@ class PullTest < ActiveSupport::TestCase
     assert_equal 'unchecked', pull.merge_status
   end
 
+  def test_create
+    assert true
+  end
+
   def test_create_minimal
     pull = Pull.new(:project_id => 1,
                     :repository_id => 10,
                     :author_id => 1,
                     :commit_base => 'master',
-                    :commit_head_revision => 'd24335d81240b6f1b98fb0a1b4fe8bb91bab7a83',
-                    :commit_head => 'develop',
                     :commit_base_revision => 'cf291599d4e280ba7c6f30481ae401c29a715f50',
+                    :commit_head => 'develop',
+                    :commit_head_revision => 'd24335d81240b6f1b98fb0a1b4fe8bb91bab7a83',
                     :subject => 'Example pull request')
 
     pull.repository.stub :branches, ['master', 'develop'] do
@@ -47,5 +55,71 @@ class PullTest < ActiveSupport::TestCase
     assert_equal IssuePriority.default.id, pull.priority_id
     assert_equal 'master', pull.commit_base
     assert_equal 'develop', pull.commit_head
+  end
+
+  def test_head_branch_should_be_validated
+    pull = Pull.new(:project_id => 1,
+                    :repository_id => 10,
+                    :author_id => 1,
+                    :commit_base => 'master',
+                    :commit_base_revision => 'cf291599d4e280ba7c6f30481ae401c29a715f50',
+                    :commit_head => 'develop',
+                    :commit_head_revision => 'd24335d81240b6f1b98fb0a1b4fe8bb91bab7a83',
+                    :subject => 'Example pull request')
+
+    pull.repository.stub :branches, ['develop'] do
+      assert_not pull.save
+    end
+  end
+
+  def test_commit_base_revision_should_be_validated
+    pull = Pull.new(:project_id => 1,
+                    :repository_id => 10,
+                    :author_id => 1,
+                    :commit_base => 'master',
+                    :commit_base_revision => nil,
+                    :commit_head => 'develop',
+                    :commit_head_revision => 'd24335d81240b6f1b98fb0a1b4fe8bb91bab7a83',
+                    :subject => 'Example pull request')
+
+    pull.repository.stub :branches, ['master', 'develop'] do
+      assert_not pull.save
+    end
+  end
+
+  def test_commit_head_revision_should_be_validated
+    pull = Pull.new(:project_id => 1,
+                    :repository_id => 10,
+                    :author_id => 1,
+                    :commit_base => 'master',
+                    :commit_base_revision => 'cf291599d4e280ba7c6f30481ae401c29a715f50',
+                    :commit_head => 'develop',
+                    :commit_head_revision => nil,
+                    :subject => 'Example pull request')
+
+    pull.repository.stub :branches, ['master', 'develop'] do
+      assert_not pull.save
+    end
+  end
+
+  def test_create_with_group_assignment
+    pull = Pull.new(:project_id => 1,
+                    :repository_id => 10,
+                    :author_id => 1,
+                    :commit_base => 'master',
+                    :commit_base_revision => 'cf291599d4e280ba7c6f30481ae401c29a715f50',
+                    :commit_head => 'develop',
+                    :commit_head_revision => 'd24335d81240b6f1b98fb0a1b4fe8bb91bab7a83',
+                    :subject => 'Example pull request',
+                    :assigned_to_id => 11)
+
+    pull.repository.stub :branches, ['master', 'develop'] do
+      assert pull.save
+    end
+
+    pull.reload
+
+    assert_kind_of Group, pull.assigned_to
+    assert_equal Group.find(11), pull.assigned_to
   end
 end
