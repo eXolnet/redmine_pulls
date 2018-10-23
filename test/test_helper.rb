65ff2198e53c40ccd45c8139940d674f8380f7a1
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-10-23 11:16:32 -0400

    Update .travis.yml to enable Code Climate code coverage reporting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,18 @@ $VERBOSE = nil # for hide ruby warnings
 
 require 'minitest/autorun'
 
+if ENV["REDMINE_PULLS_COVERAGE"]
+  require 'simplecov'
+
+  SimpleCov.root(File.expand_path(File.dirname(__FILE__) + '/..'))
+
+  SimpleCov.start 'rails' do
+    add_filter do |src|
+      ! src.filename.include? "/redmine_pulls/"
+    end
+  end
+end
+
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
